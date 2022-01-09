d3bdd2ed3c746ba68087e287e1a9577fa4a5cf1d
Haris ur Rehman <hrehman200@gmail.com>
2022-01-09 12:46:54 +0500

    Filter no dimension, changed date format

--- kameleoon.py
+++ kameleoon.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pygsheets
 import sys
 import json
+import datetime
 
 CLIENT_ID = 'xxx'
 CLIENT_SECRET = 'xxx'
@@ -27,8 +28,18 @@ response = requests.post(
 json_response = response.json()
 token = json_response['access_token']
 
+
 def condition(dic):
     return dic['name'] == 'GOOGLE_UNIVERSAL_ANALYTICS'
+
+
+# Also for the start and end date please remove the timestamps, only put in the actual date like: 4/27/2020
+def dt_format(timestamp):
+    if timestamp == '':
+        return ''
+    timestamp = timestamp.split('T')[0]
+    dt = datetime.datetime.strptime(timestamp, '%Y-%m-%d')
+    return '{0}/{1}/{2:02}'.format(dt.month, dt.day, dt.year % 100)
 
 
 for page in range(1, 2):
@@ -40,26 +51,31 @@ for page in range(1, 2):
         },
         params={
             'page': page,
-            'perPage': 200, # maximum per page 200
+            'perPage': 200,  # maximum per page 200
             'filter': '[{"field":"status","operator":"EQUAL","parameters":["'+STATUS_TO_FETCH+'"]}]'
         }
     )
     json_response = response.json()
-    
+
     print(page)
 
     with open("out.log", "w", encoding="utf-8") as f:
         f.write(json.dumps(json_response))
 
     for row in json_response:
+
+        # For Kameleoon Experiments please filter out experiments that do not have a dimension associated
+        if len(row['trackingTools']) == 0 or 'universalAnalyticsDimension' not in row['trackingTools'][0]:
+            continue
+
         item = {
             'Kameleoon Test ID': row['id'],
             'Test Name': row['name'],
-            'Custom Dimension': row['trackingTools'][0]['universalAnalyticsDimension'] if len(row['trackingTools']) > 0 and 'universalAnalyticsDimension' in row['trackingTools'][0] else '',
-            #'Start Date': row['schedules'][0]['dateStart'] if len(row['schedules']) > 0 and 'dateStart' in row['schedules'][0] else '',
-            #'End Date': row['schedules'][0]['dateEnd'] if len(row['schedules']) > 0 and 'dateEnd' in row['schedules'][0] else '',
-            'Start Date': row['dateStarted'],
-            'End Date': row['dateEnded'] if 'dateEnded' in row else '',
+            'Custom Dimension': row['trackingTools'][0]['universalAnalyticsDimension'],
+            # 'Start Date': row['schedules'][0]['dateStart'] if len(row['schedules']) > 0 and 'dateStart' in row['schedules'][0] else '',
+            # 'End Date': row['schedules'][0]['dateEnd'] if len(row['schedules']) > 0 and 'dateEnd' in row['schedules'][0] else '',
+            'Start Date': dt_format(row['dateStarted']),
+            'End Date': dt_format(row['dateEnded'] if 'dateEnded' in row else ''),
             'Site ID': row['siteId'],
             'Domain': row['baseURL'] if 'baseURL' in row else ''
         }
